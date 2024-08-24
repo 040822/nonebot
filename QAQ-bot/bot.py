2e2894fc1b7eea44190db948040cf8000343c51b
040822 <wxflwyr@126.com>
2024-08-25 00:51:45 +0800

    创建weather插件

--- QAQ-bot/bot.py
+++ QAQ-bot/bot.py
@@ -10,8 +10,8 @@ driver.register_adapter(ConsoleAdapter)
 
 # 在这里加载插件
 nonebot.load_builtin_plugins("echo")  # 内置插件
-# nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
-# nonebot.load_plugins("awesome_bot/plugins")  # 本地插件
+nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
+nonebot.load_plugins("QAQ_bot/plugins")  # 本地插件 加载传入插件目录中的所有插件
 
 if __name__ == "__main__":
     nonebot.run()
